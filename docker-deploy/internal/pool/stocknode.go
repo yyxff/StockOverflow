470fc167a7eb80cc511a8df122446292a748136c
yyxff <salmontwosday@gmail.com>
2025-03-31 19:34:49 -0400

    done node with rwlock

--- docker-deploy/internal/pool/stocknode.go
+++ docker-deploy/internal/pool/stocknode.go
@@ -1,5 +1,7 @@
 package pool
 
+import "sync"
+
 // a stock node is a trading room for a specific stock
 type StockNode struct {
 	// name
@@ -12,6 +14,9 @@ type StockNode struct {
 	// buyers and sellers heap
 	sellers SellerHeap
 	buyers  BuyerHeap
+
+	// rw lock
+	rw sync.RWMutex
 }
 
 // new
@@ -23,4 +28,34 @@ func NewStockNode(symbol string) *StockNode {
 		sellers: SellerHeap{},
 		buyers:  BuyerHeap{},
 	}
+}
+
+// get read lock
+func (node *StockNode) RLock() {
+	node.rw.RLock()
+}
+
+// get write lock
+func (node *StockNode) WLock() {
+	node.rw.Lock()
+}
+
+// unlock read
+func (node *StockNode) RUnlock() {
+	node.rw.RUnlock()
+}
+
+// unlock write
+func (node *StockNode) Unlock() {
+	node.rw.Unlock()
+}
+
+// get buyerheap
+func (node *StockNode) GetBuyers() *BuyerHeap {
+	return &node.buyers
+}
+
+// get sellerheap
+func (node *StockNode) GetSellers() *SellerHeap {
+	return &node.sellers
 }
